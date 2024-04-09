139fafd917ff2b3555f9a51adabce41d184be78e
tea5436 <158519375+tea5436@users.noreply.github.com>
2024-04-09 11:06:21 +0000

    [Finish] 3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,7 +24,7 @@ class ListsController < ApplicationController
 def destroy
     list = List.find(params[:id])  
     list.destroy  
-    redirect_to lists_path
+    redirect_to '/lists'
 end
 
   def update
