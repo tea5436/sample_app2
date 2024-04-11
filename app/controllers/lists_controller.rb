02c1239284a35380dcf4c6ba72b8da5ff58efb77
tea5436 <158519375+tea5436@users.noreply.github.com>
2024-04-11 12:14:06 +0000

    [Finish] 3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :index
+    end  
   end
 
   def index
@@ -21,11 +24,13 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
-def destroy
-    list = List.find(params[:id])  
-    list.destroy  
-    redirect_to '/lists'
-end
+  def destroy
+      list = List.find(params[:id])  
+      list.destroy  
+      redirect_to '/lists'
+  end
+
+
 
   def update
     list = List.find(params[:id])
